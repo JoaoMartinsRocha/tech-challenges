1321f8552d2addfa4ebcd12939f4379171907f65
JoaoMartinsRocha <124220623+JoaoMartinsRocha@users.noreply.github.com>
2023-04-30 17:37:05 +0000

    Insomnia is working. I understand the get and post requests but I can't figure out how to add an entry to the person table within the post method.

--- src/app.js
+++ src/app.js
@@ -8,28 +8,35 @@ const port = 3000
 
 const personRepository = AppDataSource.getRepository(Person)
 
-app.get('/', (req, res) => {
+//app.get('/', (req, res) => {
     //let readme = './README.md';
     //let output = fs.readFileSync(readme, 'utf8');
-    let output = Person
-    res.send(marked(output.toString()));
+    //res.send(marked(output.toString()));
+//})
+
+
+
+//app.get('/persons', async (req, res) => {
+  //  let persons = await personRepository.find({
+    //    relations: {
+           // roles: true
+      //  }   
+    //})
+    //res.send(persons)
+//})
+
+// testar post request
+app.post('/persons', (req, res) => {
+    res.send("POST Request Called")
 })
 
-app.get('/persons', async (req, res) => {
-    let persons = await personRepository.find({
-        relations: {
-            roles: true
-        }   
-    })
-    res.send(persons)
-})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
-// Return just the first names? or should I return other things too? and should this be a function by itself or part of the object?
-function GET() {
-    return Person.options.columns.first_name
-}
+
+
+
+
 
 
--- src/entity/Person.js
+++ src/entity/Person.js
@@ -1,47 +1,57 @@
-const { Migration } = require('typeorm')
+const AppDataSource = require('./datasource/datasource')
+const Person = require('./entity/Person.js')
 
-const EntitySchema = require('typeorm').EntitySchema
+//const Role = require('./entity/Role.js')
 
-const Person = new EntitySchema({
-    name: 'Person',
-    tableName: 'person',
-    columns: {
-        id: {
-            primary: true,
-            type: "int",
-            generated: true,
-        },
-        first_name: {
-            type: "varchar",
-        },
-        last_name: {
-            type: "varchar",
-        },
-        email: {
-            type: "varchar",
-        },
-    },
-    relations: {
-        roles: {
-            name: 'roles',
-            type: 'many-to-many',
-            target: 'role',
-            joinTable: {
-                name: 'persons_roles',
-                joinColumn: {
-                    name: 'person_id'
-                },
-                inverseJoinColumn: {
-                    name: 'role_id'
-                },
-            },
-            inverseSide: 'person',
-            cascade: true
-        },
-    },
+const fs = require("fs");
+const { marked } = require('marked');
+const express = require('express')
+const app = express()
+const port = 3000
+
+const personRepository = AppDataSource.getRepository(Person)
+
+//const roleRepository = AppDataSource.getRepository(Role)
+
+app.get('/', (req, res) => {
+    let readme = './README.md';
+    let output = fs.readFileSync(readme, 'utf8');
+    
+    res.send(marked(output.toString()));
+})
+
+app.get('/persons', async (req, res) => {
+    let persons = await personRepository.find({
+        relations: {
+            roles: true
+        }   
+    })
+    res.send(persons)
+})
+
+// Still can't figure out how to create a new entry annd add it to person 
+app.post('/persons', (req, res) => {
+    console.log("Post request called") // this works
+    //let persons = await personRepository.find({
+    //     relations: {
+    //         roles: true
+    //     }   
+    // })
+    // res.send(persons)
     
 })
 
+// Get for roles, doesn't work --> Error: Server returned nothing (no headers, no data)
+// app.get('/roles', async (req, res) => {
+//     let roles = await roleRepository.find({
+//         relations: {
+//             roles: true
+//         }   
+//     })
+//     res.send(roles)
+// })
 
-module.exports = Person
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+})
 
